88fdbe6e407ee2e26e2b8068f3e33e9f19680bf5
batdorjbaljinnym28-web <batdorjbaljinnym.28@gmail.com>
2026-09-20 21:56:55 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="mn">
+      <head>
+       <meta name="google-site-verification" content="1eRVs49y1Ctiz7gXJpz7wxRTlNha0sLl9FtJNfogYPc" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
